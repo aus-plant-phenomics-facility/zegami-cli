9490392591e2c60ccd0afd0eeaedc6f34755cad0
Ubuntu <ubuntu@tpa-zegami.novalocal>
2020-03-03 10:56:49 +1030

    appf query builder working

--- appf-collection-builder/imageset-upload.sh
+++ appf-collection-builder/imageset-upload.sh
@@ -1,2 +1,1 @@
-#docker run -it --network="host" -v $PWD/imageset.yaml:/config.yaml -v /plantdb/ftp-2019/0000_Production_N/:/images zegami-cli
 zeg update imageset {imageset_id} --config imageset.yaml --project OVdSdE5n --token {token}
