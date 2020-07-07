1d1fb67231e009c7feca84984025403268e55ce0
Ubuntu <ubuntu@tpa-zegami.novalocal>
2020-07-07 10:37:40 +0930

    template casting

--- appf-collection-builder/template-qb.sql
+++ appf-collection-builder/template-qb.sql
@@ -9,10 +9,10 @@ from
    snapshot.water_amount AS "Water Amount",
    snapshot.weight_after AS "Weight After",
    snapshot.weight_before AS "Weight Before",
-   rgb_side_far_0_analysis."RGB_Side_Far_0 Area" + rgb_side_far_4_analysis."RGB_Side_Far_4 Area" + rgb_side_lower_0_analysis."RGB_Side_Lower_0 Area" + rgb_side_lower_4_analysis."RGB_Side_Lower_4 Area" + rgb_side_upper_0_analysis."RGB_Side_Upper_0 Area" + rgb_side_upper_4_analysis."RGB_Side_Upper_4 Area" + rgb_tv_analysis."RGB_TV Area" AS "Projected Shoot Area",
-   rgb_side_far_0_analysis."RGB_Side_Far_0 Area" + rgb_side_far_4_analysis."RGB_Side_Far_4 Area" AS "Side Far Projected Shoot Area",
-   rgb_side_lower_0_analysis."RGB_Side_Lower_0 Area" + rgb_side_lower_4_analysis."RGB_Side_Lower_4 Area" AS "Side Lower Projected Shoot Area",
-   rgb_side_upper_0_analysis."RGB_Side_Upper_0 Area" + rgb_side_upper_4_analysis."RGB_Side_Upper_4 Area" AS "Side Upper Projected Shoot Area",
+   (rgb_side_far_0_analysis."RGB_Side_Far_0 Area" + rgb_side_far_4_analysis."RGB_Side_Far_4 Area" + rgb_side_lower_0_analysis."RGB_Side_Lower_0 Area" + rgb_side_lower_4_analysis."RGB_Side_Lower_4 Area" + rgb_side_upper_0_analysis."RGB_Side_Upper_0 Area" + rgb_side_upper_4_analysis."RGB_Side_Upper_4 Area" + rgb_tv_analysis."RGB_TV Area")::real AS "Projected Shoot Area",
+   (rgb_side_far_0_analysis."RGB_Side_Far_0 Area" + rgb_side_far_4_analysis."RGB_Side_Far_4 Area")::real AS "Side Far Projected Shoot Area",
+   (rgb_side_lower_0_analysis."RGB_Side_Lower_0 Area" + rgb_side_lower_4_analysis."RGB_Side_Lower_4 Area")::real AS "Side Lower Projected Shoot Area",
+   (rgb_side_upper_0_analysis."RGB_Side_Upper_0 Area" + rgb_side_upper_4_analysis."RGB_Side_Upper_4 Area")::real AS "Side Upper Projected Shoot Area",
    substring(path_view."RGB_3D_3D_side_far_0" FROM '[\d-]+/(.*)') as "RGB_3D_3D_side_far_0",
    path_view."RGB_3D_3D_side_far_0" as "RGB_3D_3D_side_far_0_path"
 FROM
