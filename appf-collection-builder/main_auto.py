0b37d0d3e04a767244f8fd4a08c648581247bc84
George Sainsbury <george.sainsbury@gmail.com>
2020-07-09 12:41:23 +0930

    area column data type patching

--- appf-collection-builder/main_auto.py
+++ appf-collection-builder/main_auto.py
@@ -124,14 +124,15 @@ def upload_dataset_from_database(collection_obj, db_name, query, token, project)
                     "Top Projected Shoot Area"]
 
     for column in area_columns:
-        url = urllib.parse.quote_plus("zegami.com/project/{project}/datasets/{dataset_id}/columns/{column_name}/fields".format(project=project,dataset_id=collection_obj['dataset_id'],column_name=column))
+        url = urllib.parse.quote_plus("zegami.com/project/{project}/datasets/{dataset_id}/columns/{column_name}/fields".format(project=project, dataset_id=collection_obj['dataset_id'], column_name=column))
 
-        data = {"zegami:schema": { "datatype": "integer" }}
+        data = {"zegami:schema": {"datatype": "integer"}}
 
         headers = {'Content-type': 'application/json', 'Authorization': 'Bearer {}'.format(token)}
 
         response = requests.patch("https://{}".format(url), json=data, headers=headers)
 
+        print(url)
         print(response)
 
 
