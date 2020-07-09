01c407be08fa977b78045d5bbb4c1504b3b8331b
George Sainsbury <george.sainsbury@gmail.com>
2020-07-09 12:08:16 +0930

    area column data type patching

--- appf-collection-builder/template-qb.sql
+++ appf-collection-builder/template-qb.sql
@@ -13,6 +13,7 @@ from
    (rgb_side_far_0_analysis."RGB_Side_Far_0 Area" + rgb_side_far_4_analysis."RGB_Side_Far_4 Area")::real AS "Side Far Projected Shoot Area",
    (rgb_side_lower_0_analysis."RGB_Side_Lower_0 Area" + rgb_side_lower_4_analysis."RGB_Side_Lower_4 Area")::real AS "Side Lower Projected Shoot Area",
    (rgb_side_upper_0_analysis."RGB_Side_Upper_0 Area" + rgb_side_upper_4_analysis."RGB_Side_Upper_4 Area")::real AS "Side Upper Projected Shoot Area",
+   rgb_tv_analysis."RGB_TV Area" as "Top Projected Shoot Area",
    substring(path_view."RGB_3D_3D_side_far_0" FROM '[\d-]+/(.*)') as "RGB_3D_3D_side_far_0",
    path_view."RGB_3D_3D_side_far_0" as "RGB_3D_3D_side_far_0_path"
 FROM
