371e27bab0c370331d7f09cafad8a2939e73a089
Ubuntu <ubuntu@tpa-zegami.novalocal>
2020-07-09 10:49:59 +0930

    new projects

--- appf-collection-builder/main_auto.py
+++ appf-collection-builder/main_auto.py
@@ -227,7 +227,7 @@ def main():
     if len(sys.argv) > 1:
         if sys.argv[1] == "auto":
 
-            projects = ["iCFLiDym"]#, "OVdSdE5n"]
+            projects = ["iCFLiDym", "OVdSdE5n"]
 
             for project in projects:
 
